9c8ff3ec30e94c7a34f9e5aa87bc6177a9044adc
RoaiaElemam <mroaia219@gmail.com>
2023-09-18 09:59:23 +0300

    finshed 6

---
+++ 06-task6.py
@@ -0,0 +1,9 @@
+'''Create a function that take a sentence and word and remove the word from the sentencs '''
+
+def sen (sentence,word):
+  sentence = sentence.replace(word)
+  print(sentence)
+
+sentence = input("enter the sentence.....")
+word = input("enter the word.....") 
+print(sen(sentence,word))
